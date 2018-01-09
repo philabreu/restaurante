95de2d8b684db2012cf33163fa2e4edc4865cb13
Filipe Abreu Bezerra <bezzerra.filipe@gmail.com>
2018-01-08 21:21:36 -0300

    Tratamento de exceptions

--- src/app/App.java
+++ src/app/App.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -33,82 +34,91 @@ public class App {
 		restaurantes.add(setimo);
 
 		Scanner scanner = new Scanner(System.in);
-		
 
 		while (opcao != 5) {
 
 			System.out.println(
 					"O que deseja fazer? \n \n1 - Votar\n2 - Resultado parcial\n3 - Encerrar votação\n4 - Nova votação\n5 - Sair");
 
-			opcao = scanner.nextInt();
+			try {
 
-			switch (opcao) {
+				opcao = scanner.nextInt();
 
-			/*Votar na lista de restaurantes*/
-			case 1:
-				if (votacaoEncerrada) {
-					System.out.println("Votação de hoje finalizada. Tente amanhã \n");
-				} else {
+				switch (opcao) {
 
-					int voto = 0;
-					ArrayList<Restaurante> listaRestaurantes = new ArrayList<Restaurante>();
+				/* Votar na lista de restaurantes */
+				case 1:
+					if (votacaoEncerrada) {
+						System.out.println("Votação de hoje finalizada. Tente amanhã \n");
+					} else {
 
-					System.out.println("Selecione um restaurante da nossa rede:");
-					for (Restaurante restaurante : restaurantes) {
+						int voto = 0;
+						ArrayList<Restaurante> listaRestaurantes = new ArrayList<Restaurante>();
 
-						if (!restaurante.getEscolhido()) {
-							listaRestaurantes.add(restaurante);
-						} else {
-							System.out.println("Não é possivel mais votar.");
+						System.out.println("Selecione um restaurante da nossa rede:");
+						for (Restaurante restaurante : restaurantes) {
+
+							if (!restaurante.getEscolhido()) {
+								listaRestaurantes.add(restaurante);
+							} else {
+								System.out.println("Não é possivel mais votar.");
+							}
+						}
+
+						restaurantes = (List<Restaurante>) listaRestaurantes.clone();
+
+						for (int contador = 0; contador <= restaurantes.size() - 1; contador++) {
+
+							System.out.println(Integer.toString(++contador) + " - "
+									+ restaurantes.get(--contador).getNome());
+						}
+
+						try {
+
+							voto = scanner.nextInt();
+
+							if (voto > restaurantes.size()) {
+								System.out.println("Digite uma opção de restaurante válida.");
+								continue;
+							}
+							restaurantes.get(--voto).votar();
+						} catch (Exception exception) {
+							throw new InputMismatchException("Digite uma opção de restaurante válida.");
 						}
 					}
 
-					restaurantes = (List<Restaurante>) listaRestaurantes.clone();
+					break;
 
-					for (int contador = 0; contador <= restaurantes.size() - 1; contador++) {
+				/* Apuração parcial */
+				case 2:
+					Restaurante parcial = new Restaurante();
+					parcial.apurar(restaurantes, false);
 
-						System.out.println(Integer.toString(++contador) + " - "
-								+ restaurantes.get(--contador).getNome());
-					}
+					break;
 
-					voto = scanner.nextInt();
+				/* Encerra votação e apuração final */
+				case 3:
+					Restaurante encerrado = new Restaurante();
+					encerrado.apurar(restaurantes, true);
+					votacaoEncerrada = true;
 
-					if (voto > 5 || voto < 1) {
-						System.out.println("Digite uma opção de restaurante válida.");
-						continue;
-					}
-					restaurantes.get(--voto).votar();
+					break;
+
+				/* Cria uma nova votação */
+				case 4:
+					votacaoEncerrada = false;
+
+					/* Sai do sistema */
+				case 5:
+					System.out.println("Até mais! \n");
+					break;
+
+				default:
+
+					System.out.println("Digite uma opção válida.");
 				}
-
-				break;
-
-				/*Apuração parcial*/
-			case 2:
-				Restaurante parcial = new Restaurante();
-				parcial.apurar(restaurantes, false);
-
-				break;
-
-			/* Encerra votação e apuração final */
-			case 3:
-				Restaurante encerrado = new Restaurante();
-				encerrado.apurar(restaurantes, true);
-				votacaoEncerrada = true;
-
-				break;
-
-				/*Cria uma nova votação*/
-			case 4:
-				votacaoEncerrada = false;
-
-				/* Sai do sistema */
-			case 5:
-				System.out.println("Até mais! \n");
-				break;
-
-			default:
-
-				System.out.println("Digite uma opção válida.");
+			} catch (Exception exception) {
+				throw new InputMismatchException("Digite uma opção de restaurante válida.");
 			}
 		}
 	}
